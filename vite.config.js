225c367c3bb52438f81a23277419180418416010
Muntazir Haider <111944042+MuntazirHaider@users.noreply.github.com>
2024-12-13 17:17:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,25 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build', 
+    outDir: 'dist', 
+     rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], 
+        },
+      },
+   },
   },
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://urbansquare-server.onrender.com',
-        secure: false,
-      },
+   proxy: {
+    '/api': {
+      target: 'https://urbansquare-server.onrender.com',
+      changeOrigin: true, // Adjust the host header to match the target
+      secure: false,      // Disable SSL verification if needed
+      rewrite: (path) => path.replace(/^\/api/, ''), // Remove '/api' prefix if necessary
     },
+}
+
   },
 
   plugins: [react()],
